ed11c07b0af7d172d7b8f6913044d9ff9c63e2c8
Anita <24868560+g-anita@users.noreply.github.com>
2018-12-19 00:43:40 -0500

    Updates

---
+++ precache-manifest.97a5576fa4699cae231ce09ac51b16d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0783c0952ebe7081890",
+    "url": "/react-stopwatch-app/static/js/runtime~main.a0783c09.js"
+  },
+  {
+    "revision": "09dff16ea417686a1965",
+    "url": "/react-stopwatch-app/static/js/main.09dff16e.chunk.js"
+  },
+  {
+    "revision": "967c3ca6db0baac7de4d",
+    "url": "/react-stopwatch-app/static/js/1.967c3ca6.chunk.js"
+  },
+  {
+    "revision": "09dff16ea417686a1965",
+    "url": "/react-stopwatch-app/static/css/main.d6f22b00.chunk.css"
+  },
+  {
+    "revision": "0d2827a9a00a67d24c9f0bf24100407b",
+    "url": "/react-stopwatch-app/index.html"
+  }
+];
